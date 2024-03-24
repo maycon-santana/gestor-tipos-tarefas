3e8018adba0cc1a12afed1f9971fac88fb62038f
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2024-03-24 19:22:58 -0300

    Criando a tabela de usuarios

---
+++ src/database/index.js
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize";
+
+import config from "../config/database.cjs";
+
+// import Model from "../models/xxxx";
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(config);
+    this.init();
+    this.associate();
+  }
+
+  init() {
+    models.forEach((model) => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach((model) => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
+  }
+
+}
+
+export default new Database();
--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
-import AdminJS from 'adminjs';
-import AdminJSExpress from '@adminjs/express';
-import express from 'express';
+import "dotenv/config";
+
+import "../src/config/database.cjs";
+
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express";
+import AdminJSSequelize from "@adminjs/sequelize";
+import express from "express";
+
+AdminJS.registerAdapter(AdminJSSequelize);
 
 const app = express();
 
 const adminJS = new AdminJS({
     databases: [],
-    rootPath: '/admin',
+    rootPath: "/admin",
     resources: [],
 });
 
@@ -14,5 +21,5 @@ const router = AdminJSExpress.buildRouter(adminJS);
 
 app.use(adminJS.options.rootPath, router);
 app.listen(5000, () => {
-    console.log('AdminJS rodando na página https://localhost:5000/admin');
+    console.log("AdminJS rodando na página https://localhost:5000/admin");
 });
